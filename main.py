05ab5605bfc3349cf5ec7099e42b62ca5a916317
iridiumR <i@iridium.cyou>
2022-08-10 21:01:46 +0800

    feat: 增加指定配置文件地址的功能
    Archlinux 包的默认配置文件位于 /usr/share/xdao/config.ini，不符合 XDG
    目录规范，增加指定配置文件位置功能以便重新指定配置文件于/home/ir/.config 中.
    添加新的依赖库: click

--- main.py
+++ main.py
@@ -5,36 +5,47 @@ from prompt_toolkit import Application
 from prompt_toolkit.output.color_depth import ColorDepth
 from prompt_toolkit.styles import Style
 
-xdnmb.action.loadForumGroup()
+import click
+@click.command()
+@click.option('--conf_path', default=xdnmb.globals.APP_PATH, help='Path of config file.')
 
-Application(
-    layout=xdnmb.globals.layout,
-    key_bindings=xdnmb.globals.keyBinding,
-    full_screen=True,
-    style=Style.from_dict({
-        'nav': 'bg:#ffffff #cc0000',
-        'nav button': '#0077dd',
-        'nav button.focused': 'bg:#bbeeff',
-        'divide': 'bg:#ffffee #000000',
-        'content': 'bg:#ffffee #800000',
-        'content-rev': 'bg:#800000 #ffffee',
-        'title': '#cc1105 bold',
-        'name': '#117743 bold',
-        'sage': '#d85030',
-        'tips': '#707070',
-        'admin': '#ff0000',
-        'name-admin': '#444444',
-        'name-po': '#2d7091',
-        'reply': 'bg:#f0e0d6',
-        'reference': '#789922',
-        'content button': '#0077dd',
-        'content button.focused': 'bg:#bbeeff',
-        'form-label': 'bg:#eeaa88',
-        'form-textarea': 'bg:#ffffff',
-    }),
-    color_depth=(
-        ColorDepth.DEPTH_1_BIT
-        if xdnmb.globals.config['Config'].getboolean('Monochrome') else
-        ColorDepth.DEPTH_24_BIT
-    ),
-).run()
+def main(conf_path):
+    
+    xdnmb.globals.APP_PATH = conf_path
+    xdnmb.action.loadForumGroup()
+
+    Application(
+        layout=xdnmb.globals.layout,
+        key_bindings=xdnmb.globals.keyBinding,
+        full_screen=True,
+        style=Style.from_dict({
+            'nav': 'bg:#ffffff #cc0000',
+            'nav button': '#0077dd',
+            'nav button.focused': 'bg:#bbeeff',
+            'divide': 'bg:#ffffee #000000',
+            'content': 'bg:#ffffee #800000',
+            'content-rev': 'bg:#800000 #ffffee',
+            'title': '#cc1105 bold',
+            'name': '#117743 bold',
+            'sage': '#d85030',
+            'tips': '#707070',
+            'admin': '#ff0000',
+            'name-admin': '#444444',
+            'name-po': '#2d7091',
+            'reply': 'bg:#f0e0d6',
+            'reference': '#789922',
+            'content button': '#0077dd',
+            'content button.focused': 'bg:#bbeeff',
+            'form-label': 'bg:#eeaa88',
+            'form-textarea': 'bg:#ffffff',
+        }),
+        color_depth=(
+            ColorDepth.DEPTH_1_BIT
+            if xdnmb.globals.config['Config'].getboolean('Monochrome') else
+            ColorDepth.DEPTH_24_BIT
+        ),
+    ).run()
+
+if __name__ == '__main__':
+    main()
+    
